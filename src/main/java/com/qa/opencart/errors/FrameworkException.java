5971050a376a79a0c12a7c14e2be96411c15d186
Nitya <nitya@gmail.com>
2023-04-02 11:37:33 +0530

    Custome Error class pushed

---
+++ src/main/java/com/qa/opencart/errors/FrameworkException.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.errors;
+
+public class FrameworkException extends RuntimeException {
+	
+	public FrameworkException() {
+		super();
+	}
+	
+	public FrameworkException(String message) {
+		super(message);
+	}
+
+}
